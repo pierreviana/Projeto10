cb18ed3d41c1850bdd9d544846c68925d2dc4e97
Pierre Viana <pierreviana@gmail.com>
2024-10-15 17:52:22 -0400

    versão 3.0 do projeto

---
+++ Projeto10/src/main/java/com/mycompany/projeto10/Classe4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projeto10;
+
+/**
+ *
+ * @author mayunia-sti
+ */
+public class Classe4 {
+    public static void main(String[] args) {
+        
+        System.out.println("Tesdsjdskjds");
+    }
+}
